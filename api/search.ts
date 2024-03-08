b204815a60ead83fd81c01caa976eacccb592805
phusitrubun <158720692+phusitrubun@users.noreply.github.com>
2024-03-09 00:56:00 +0700

    #

--- api/search.ts
+++ api/search.ts
@@ -7,37 +7,48 @@ router.get("/search", (req, res) => {
     conn.query(
       "SELECT * FROM Movie WHERE name LIKE ?",
       ["%" + req.query.name + "%"],
-      (err, Movie_result, fields) => {
+      (err, Movie_result) => {
         if (err) throw err;
-  
+
         conn.query(
           "SELECT * FROM person INNER JOIN Stars ON person.pid = Stars.pid WHERE Stars.mid IN (SELECT Mid FROM Movie WHERE name LIKE ?)",
           ["%" + req.query.name + "%"],
-          (err, Stars_result, fields) => {
+          (err, Stars_result) => {
             if (err) throw err;
             conn.query(
               "SELECT * FROM person INNER JOIN creators ON person.pid = creators.pid WHERE creators.mid IN (SELECT mid FROM Movie WHERE name LIKE ?)",
               ["%" + req.query.name + "%"],
-              (err, Creators_Result, fields) => {
+              (err, Creators_Result) => {
                 if (err) throw err;
-        
-  
-                // Send the response with all data
-                res.json({
-                  Movie: Movie_result.map((movie: any) => ({
-                    ...movie,
-                    Stars: Stars_result.filter(
-                      (star: any) => star.mid === movie.mid
-                    ),
-                    creators: Creators_Result.filter(
-                      (creator: any) => creator.mid === movie.mid
-                    ),
+
+              // Send the response with all data
+              res.json({
+                Movie: Movie_result.map((movie: any) => ({
+                  ...movie,
+                  Stars: Stars_result.filter(
+                    (star: any) => star.mid === movie.mid
+                  ).map((star: any) => ({
+                    pid: star.pid,
+                    name: star.name,
+                    birthdate: star.birthdate,
+                    detail: star.detail,
+                    rank: star.rank,
                   })),
-                });
-              }
-            );
-          }
-        );
-      }
-    );
-  });
+                  creators: Creators_Result.filter(
+                    (creator: any) => creator.mid === movie.mid
+                  ).map((creator: any) => ({
+                    cid: creator.cid,
+                    name: creator.name,
+                    birthdate: creator.birthdate,
+                    detail: creator.detail,
+                    rank: creator.rank,
+                  })),
+                })),
+              });
+            }
+          );
+        }
+      );
+    }
+  );
+});
